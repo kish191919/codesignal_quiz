456bdfb807815a53d5e5eae5cace96106fb0c3e4
sunghwanki <kish1919@gmail.com>
2025-05-28 13:04:47 -0400

    5/28/2025

--- Fundamental_Coding_with_Python/Course_5_5.py
+++ Fundamental_Coding_with_Python/Course_5_5.py
@@ -23,9 +23,86 @@ def solution(balloons):
 balloons = [4, 6, 2]
 solution(balloons)
 
-# Step 0: [4, 6, 2]
-# Step 1: [2, 8, 2]
-# Step 2: [3, 4, 5]
-# Step 3: [4, 2, 6]
-# Step 4: [2, 5, 5]
-# Step 5: [4, 2, 6]
+# Quiz 1
+# https://codesignal.com/learn/course/94/unit/5/practice/1
+
+def solution1(s):
+    # TODO: implement the solution here
+    s = list(s)
+    ans = ""
+  
+    while 0 < len(s): 
+        temp = []
+        if len(s) % 2 == 1 and len(s) != 1:
+            for i in range(0, len(s)-1, 2): 
+                temp.append((s[i],s[i+1]))
+            result = []
+            for a, b in temp:
+                if a == b:
+                    result.append(a)
+                    ans += b
+                else:
+                    c = max(ord(a), ord(b))
+                    result.append(chr(c))
+                    ans += chr(min(ord(a), ord(b)))
+            result.append(s[-1])
+            print(result)
+            s = result
+
+        elif len(s) % 2 == 0:
+            for i in range(0, len(s)-1, 2): 
+                temp.append((s[i],s[i+1]))
+            result = []
+            for a, b in temp:
+                if a == b:
+                    result.append(a)
+                    ans += b
+                else:
+                    c = max(ord(a), ord(b))
+                    result.append(chr(c))
+                    ans += chr(min(ord(a), ord(b)))
+            print(result)
+            s = result
+        elif len(s) == 1:
+            result = []
+            ans += s[0]
+            result.append(s[0])
+            s = []
+
+    print(list(ans))
+    return list(ans)
+
+s='BCAAB'
+solution1(s)
+#  ['B', 'A', 'A', 'B', 'C']
+
+
+
+def solution1A(s):
+    removed = []
+    while s:
+        new_s = ''
+        i = 0
+        while i < len(s) - 1:
+            a, b = s[i], s[i + 1]
+            if a <= b:
+                removed.append(a)
+                new_s += b
+            else:
+                removed.append(b)
+                new_s += a
+            i += 2
+        if i == len(s) - 1:
+            # 문자열 길이가 홀수일 경우 마지막 문자 남음
+            new_s += s[-1]
+        if len(new_s) == 1:
+            # 마지막 남은 문자도 제거
+            removed.append(new_s[0])
+            break
+        s = new_s
+    print(removed)
+    return removed
+
+s='BCAAB'
+solution1A(s)
+#  ['B', 'A', 'A', 'B', 'C']
